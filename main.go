7ea787168342f0e4c035bf7286fc073c5803d7ed
MOHAMMAD Taushif Reza <112973122+TaushifReza@users.noreply.github.com>
2025-11-15 04:45:35 +0545

    add event model and implement event retrieval and creation endpoints

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/TaushifReza/go-event-booking-api/models"
 	"github.com/gin-gonic/gin"
 )
 
@@ -10,10 +12,29 @@ func main() {
 	server := gin.Default()
 
 	server.GET("/events", getEvents)
+	server.POST("/events", createEvent)
 
 	server.Run(":8080")
 }
 
 func getEvents(c *gin.Context){
-	c.JSON(http.StatusOK, gin.H{"message": "Hello World!"})
+	events := models.GetAllEvents()
+	c.JSON(http.StatusOK, gin.H{"message": "Hello World!", "events": events})
+}
+
+func createEvent(c *gin.Context){
+	var event models.Event
+	err := c.ShouldBindJSON(&event)
+
+	if err != nil{
+		fmt.Println(event)
+		c.JSON(http.StatusBadRequest, gin.H{"error": err})
+		return
+	}
+
+	event.ID = 1
+	event.UserID = 1
+	event.Save()
+
+	c.JSON(http.StatusCreated, gin.H{"message": "Event Created", "event": event})
 }
