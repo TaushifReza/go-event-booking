9eda720f10ca6ae7e61cfe366fb16d4d516f1767
MOHAMMAD Taushif Reza <112973122+TaushifReza@users.noreply.github.com>
2025-11-17 22:56:31 +0545

    feat: implement update and delete functionality for events

--- routes/events.go
+++ routes/events.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/TaushifReza/go-event-booking-api/models"
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 )
 
 func getEvents(c *gin.Context) {
@@ -60,4 +61,77 @@ func getEvent(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, gin.H{"message": "Hello World!", "events": event})
+}
+
+func updateEvents(c *gin.Context){
+	id , err := strconv.ParseInt(c.Param("id"), 10, 64)
+	if err != nil{
+		fmt.Println(err)
+		c.JSON(http.StatusBadRequest, gin.H{"message": "Invalid id format"})
+	}
+
+	_, err = models.GetEvent(id)
+
+	if err != nil {
+		if err == sql.ErrNoRows {
+			c.JSON(http.StatusNotFound, gin.H{"error": fmt.Sprintf("Event with ID %d not found", id)})
+			return
+		}
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	var updatedEvent models.Event
+
+	err = c.ShouldBind(&updatedEvent)
+
+	if err != nil {
+		if errs, ok := err.(validator.ValidationErrors); ok {
+			out := make(map[string]string)
+			for _, e := range errs {
+				out[e.Field()] = fmt.Sprintf("%s is %s", e.Field(), e.ActualTag())
+			}
+			c.JSON(http.StatusBadRequest, gin.H{"errors": out})
+			return
+		}
+
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	updatedEvent.ID = id
+	err = updatedEvent.UpdateEvent()
+	if err != nil{
+		fmt.Println(err)
+		c.JSON(http.StatusInternalServerError, gin.H{"message": "Something went wrong."})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "Event updated successfully."})
+}
+
+func deleteEvents(c *gin.Context){
+	id , err := strconv.ParseInt(c.Param("id"), 10, 64)
+	if err != nil{
+		fmt.Println(err)
+		c.JSON(http.StatusBadRequest, gin.H{"message": "Invalid id format"})
+	}
+
+	event , err := models.GetEvent(id)
+
+	if err != nil {
+		if err == sql.ErrNoRows {
+			c.JSON(http.StatusNotFound, gin.H{"error": fmt.Sprintf("Event with ID %d not found", id)})
+			return
+		}
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	err = event.DeleteEvent()
+	if err != nil{
+		fmt.Println(err)
+		c.JSON(http.StatusInternalServerError, gin.H{"message": "Something went wrong."})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "Event deleted successfully."})
 }
--- routes/routes.go
+++ routes/routes.go
@@ -6,4 +6,6 @@ func RegisterRoutes(server *gin.Engine){
 	server.GET("/events", getEvents)
 	server.POST("/events", createEvent)
 	server.GET("/event/:id/", getEvent)
+	server.PUT("/events/:id", updateEvents)
+	server.DELETE("/events/:id", deleteEvents)
 }
