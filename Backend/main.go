6cde9424ccbcc5087cf7b2ee5e6ec32b46e31f9a
MOHAMMAD Taushif Reza <112973122+TaushifReza@users.noreply.github.com>
2025-11-22 21:29:48 +0545

    feat: add CORS support and .env example for configuration

--- Backend/main.go
+++ Backend/main.go
@@ -3,13 +3,21 @@ package main
 import (
 	"github.com/TaushifReza/go-event-booking-api/db"
 	"github.com/TaushifReza/go-event-booking-api/routes"
+	"github.com/TaushifReza/go-event-booking-api/utils"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+        panic("No .env file found (using environment variables)")
+    }
+	
 	db.InitDB()
 	server := gin.Default()
 
+	utils.SetupCors(server)
 	routes.RegisterRoutes(server)
 
 	server.Run(":8080")
